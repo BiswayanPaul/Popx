bd4110eb055563fce5c452fce43d6f4a19fe7bd8
Biswayan Paul <biswayanpaul2015@gmail.com>
2025-08-23 10:27:49 +0000

    feat: implement user registration route and controller; refactor asyncHandler utility

--- src/app.js
+++ src/app.js
@@ -12,10 +12,17 @@ app.use(cors({
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extented: true, limit: "16kb" }));
-
 app.use(express.static("public"))
 app.use(cookieParser());
 
+
+// Routes import
+import userRouter from "./routes/user.routes.js";
+
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 
 
